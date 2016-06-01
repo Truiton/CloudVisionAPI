3c5450ebc0debe505662d25dfcaf956430d5264e
Mohit Gupt <mohitgupt@gmail.com>
2016-06-01 20:31:46 +0530

    Base commit for Google Cloud Vision API on Android.

---
+++ app/src/main/java/com/truiton/cloudvisionapi/MainActivity.java
@@ -0,0 +1,230 @@
+package com.truiton.cloudvisionapi;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String API_KEY = "YOUR_API_KEY_HERE";
+    private static final int REQUEST_GALLERY_IMAGE = 10;
+    private final String LOG_TAG = "MainActivity";
+    private ImageView selectedImage;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button selectImageButton = (Button) findViewById(R.id
+                .select_image_button);
+        selectedImage = (ImageView) findViewById(R.id.selected_image);
+        resultTextView = (TextView) findViewById(R.id.result);
+
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchImagePicker();
+            }
+        });
+    }
+
+    private void launchImagePicker() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select an image"),
+                REQUEST_GALLERY_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_GALLERY_IMAGE && resultCode == RESULT_OK && data != null) {
+            uploadImage(data.getData());
+        }
+    }
+
+    public void uploadImage(Uri uri) {
+        if (uri != null) {
+            try {
+                Bitmap bitmap = resizeBitmap(
+                                MediaStore.Images.Media.getBitmap(getContentResolver(), uri));
+
+                callCloudVision(bitmap);
+                selectedImage.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, e.getMessage());
+            }
+        } else {
+            Log.e(LOG_TAG, "Null image was returned.");
+        }
+    }
+
+    private void callCloudVision(final Bitmap bitmap) throws IOException {
+        resultTextView.setText("Retrieving results from cloud");
+
+        new AsyncTask<Object, Void, String>() {
+            @Override
+            protected String doInBackground(Object... params) {
+                try {
+                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+                    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+                    builder.setVisionRequestInitializer(new
+                            VisionRequestInitializer(API_KEY));
+                    Vision vision = builder.build();
+
+                    List<Feature> featureList = new ArrayList<>();
+                    Feature labelDetection = new Feature();
+                    labelDetection.setType("LABEL_DETECTION");
+                    labelDetection.setMaxResults(10);
+                    featureList.add(labelDetection);
+
+                    Feature textDetection = new Feature();
+                    textDetection.setType("TEXT_DETECTION");
+                    textDetection.setMaxResults(10);
+                    featureList.add(textDetection);
+
+                    Feature landmarkDetection = new Feature();
+                    landmarkDetection.setType("LANDMARK_DETECTION");
+                    landmarkDetection.setMaxResults(10);
+                    featureList.add(landmarkDetection);
+
+                    List<AnnotateImageRequest> imageList = new ArrayList<>();
+                    AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+                    Image base64EncodedImage = getBase64EncodedJpeg(bitmap);
+                    annotateImageRequest.setImage(base64EncodedImage);
+                    annotateImageRequest.setFeatures(featureList);
+                    imageList.add(annotateImageRequest);
+
+                    BatchAnnotateImagesRequest batchAnnotateImagesRequest =
+                            new BatchAnnotateImagesRequest();
+                    batchAnnotateImagesRequest.setRequests(imageList);
+
+                    Vision.Images.Annotate annotateRequest =
+                            vision.images().annotate(batchAnnotateImagesRequest);
+                    // Due to a bug: requests to Vision API containing large images fail when GZipped.
+                    annotateRequest.setDisableGZipContent(true);
+                    Log.d(LOG_TAG, "sending request");
+
+                    BatchAnnotateImagesResponse response = annotateRequest.execute();
+                    return convertResponseToString(response);
+
+                } catch (GoogleJsonResponseException e) {
+                    Log.e(LOG_TAG, "Request failed: " + e.getContent());
+                } catch (IOException e) {
+                    Log.d(LOG_TAG, "Request failed: " + e.getMessage());
+                }
+                return "Cloud Vision API request failed.";
+            }
+
+            protected void onPostExecute(String result) {
+                resultTextView.setText(result);
+            }
+        }.execute();
+    }
+
+    private String convertResponseToString(BatchAnnotateImagesResponse response) {
+        StringBuilder message = new StringBuilder("Results:\n\n");
+        message.append("Labels:\n");
+        List<EntityAnnotation> labels = response.getResponses().get(0).getLabelAnnotations();
+        if (labels != null) {
+            for (EntityAnnotation label : labels) {
+                message.append(String.format(Locale.getDefault(), "%.3f: %s",
+                        label.getScore(), label.getDescription()));
+                message.append("\n");
+            }
+        } else {
+            message.append("nothing\n");
+        }
+
+        message.append("Texts:\n");
+        List<EntityAnnotation> texts = response.getResponses().get(0)
+                .getTextAnnotations();
+        if (texts != null) {
+            for (EntityAnnotation text : texts) {
+                message.append(String.format(Locale.getDefault(), "%s: %s",
+                        text.getLocale(), text.getDescription()));
+                message.append("\n");
+            }
+        } else {
+            message.append("nothing\n");
+        }
+
+        message.append("Landmarks:\n");
+        List<EntityAnnotation> landmarks = response.getResponses().get(0)
+                .getLandmarkAnnotations();
+        if (landmarks != null) {
+            for (EntityAnnotation landmark : landmarks) {
+                message.append(String.format(Locale.getDefault(), "%.3f: %s",
+                        landmark.getScore(), landmark.getDescription()));
+                message.append("\n");
+            }
+        } else {
+            message.append("nothing\n");
+        }
+
+        return message.toString();
+    }
+
+    public Bitmap resizeBitmap(Bitmap bitmap) {
+
+        int maxDimension = 1024;
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
+    public Image getBase64EncodedJpeg(Bitmap bitmap) {
+        Image image = new Image();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+        image.encodeContent(imageBytes);
+        return image;
+    }
+}

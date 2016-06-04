1c3f5e83a1eee66721e988e8ea9f11c86b8b0dc4
Mohit Gupt <mohitgupt@gmail.com>
2016-06-04 22:45:10 +0530

    Implemented the OAuth 2.0 implementation for Google Cloud Vision API.

---
+++ app/src/main/java/com/truiton/cloudvisionapi/GetTokenTask.java
@@ -0,0 +1,62 @@
+package com.truiton.cloudvisionapi;
+
+import android.accounts.Account;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+
+import java.io.IOException;
+
+/**
+ * Created by MG on 04-06-2016.
+ */
+public class GetTokenTask extends AsyncTask<Void, Void, Void> {
+    Activity mActivity;
+    String mScope;
+    Account mAccount;
+    int mRequestCode;
+
+    GetTokenTask(Activity activity, Account account, String scope, int requestCode) {
+        this.mActivity = activity;
+        this.mScope = scope;
+        this.mAccount = account;
+        this.mRequestCode = requestCode;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            String token = fetchToken();
+            if (token != null) {
+                ((MainActivity)mActivity).onTokenReceived(token);
+            }
+        } catch (IOException e) {
+            Toast.makeText(mActivity, "Some error occurred", Toast.LENGTH_SHORT)
+                    .show();
+        }
+        return null;
+    }
+
+    /**
+     * Gets an authentication token from Google and handles any
+     * GoogleAuthException that may occur.
+     */
+    protected String fetchToken() throws IOException {
+        String accessToken;
+        try {
+            accessToken = GoogleAuthUtil.getToken(mActivity, mAccount, mScope);
+            GoogleAuthUtil.clearToken (mActivity, accessToken); // used to remove stale tokens.
+            accessToken = GoogleAuthUtil.getToken(mActivity, mAccount, mScope);
+            return accessToken;
+        } catch (UserRecoverableAuthException userRecoverableException) {
+            mActivity.startActivityForResult(userRecoverableException.getIntent(), mRequestCode);
+        } catch (GoogleAuthException fatalException) {
+            fatalException.printStackTrace();
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/truiton/cloudvisionapi/MainActivity.java
+++ app/src/main/java/com/truiton/cloudvisionapi/MainActivity.java
@@ -1,10 +1,16 @@
 package com.truiton.cloudvisionapi;
 
+import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,14 +18,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
 import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.vision.v1.Vision;
-import com.google.api.services.vision.v1.VisionRequestInitializer;
 import com.google.api.services.vision.v1.model.AnnotateImageRequest;
 import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
 import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
@@ -34,11 +43,15 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String API_KEY = "YOUR_API_KEY_HERE";
-    private static final int REQUEST_GALLERY_IMAGE = 10;
+    private static String accessToken;
+    static final int REQUEST_GALLERY_IMAGE = 10;
+    static final int REQUEST_CODE_PICK_ACCOUNT = 11;
+    static final int REQUEST_ACCOUNT_AUTHORIZATION = 12;
+    static final int REQUEST_PERMISSIONS = 13;
     private final String LOG_TAG = "MainActivity";
     private ImageView selectedImage;
     private TextView resultTextView;
+    Account mAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +65,9 @@ public class MainActivity extends AppCompatActivity {
         selectImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchImagePicker();
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.GET_ACCOUNTS},
+                        REQUEST_PERMISSIONS);
             }
         });
     }
@@ -66,10 +81,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getAuthToken();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_GALLERY_IMAGE && resultCode == RESULT_OK && data != null) {
             uploadImage(data.getData());
+        } else if (requestCode == REQUEST_CODE_PICK_ACCOUNT) {
+            if (resultCode == RESULT_OK) {
+                String email = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                AccountManager am = AccountManager.get(this);
+                Account[] accounts = am.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+                for (Account account : accounts) {
+                    if (account.name.equals(email)) {
+                        mAccount = account;
+                        break;
+                    }
+                }
+                getAuthToken();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "No Account Selected", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        } else if (requestCode == REQUEST_ACCOUNT_AUTHORIZATION) {
+            if (resultCode == RESULT_OK) {
+                Bundle extra = data.getExtras();
+                onTokenReceived(extra.getString("authtoken"));
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Authorization Failed", Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
     }
 
@@ -78,10 +131,8 @@ public class MainActivity extends AppCompatActivity {
             try {
                 Bitmap bitmap = resizeBitmap(
                                 MediaStore.Images.Media.getBitmap(getContentResolver(), uri));
-
                 callCloudVision(bitmap);
                 selectedImage.setImageBitmap(bitmap);
-
             } catch (IOException e) {
                 Log.e(LOG_TAG, e.getMessage());
             }
@@ -97,12 +148,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected String doInBackground(Object... params) {
                 try {
+                    GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken);
                     HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
                     JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
 
-                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
-                    builder.setVisionRequestInitializer(new
-                            VisionRequestInitializer(API_KEY));
+                    Vision.Builder builder = new Vision.Builder
+                            (httpTransport, jsonFactory, credential);
                     Vision vision = builder.build();
 
                     List<Feature> featureList = new ArrayList<>();
@@ -226,5 +277,27 @@ public class MainActivity extends AppCompatActivity {
         byte[] imageBytes = byteArrayOutputStream.toByteArray();
         image.encodeContent(imageBytes);
         return image;
+    }
+
+    private void pickUserAccount() {
+        String[] accountTypes = new String[]{GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE};
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                accountTypes, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
+    }
+
+    private void getAuthToken() {
+        String SCOPE = "oauth2:https://www.googleapis.com/auth/cloud-platform";
+        if (mAccount == null) {
+            pickUserAccount();
+        } else {
+            new GetTokenTask(MainActivity.this, mAccount, SCOPE, REQUEST_ACCOUNT_AUTHORIZATION)
+                    .execute();
+        }
+    }
+
+    public void onTokenReceived(String token){
+        accessToken = token;
+        launchImagePicker();
     }
 }
